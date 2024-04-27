e5378154981faaf5a686772f396ebc75821a9a47
AhmAyman <Ahm.Ayman4566@gmail.com>
2024-04-27 17:36:56 +0300

    Added JoinUs, JoinCaptain, Mapview and some modifications for responsiveness

--- AidDrive/src/App.jsx
+++ AidDrive/src/App.jsx
@@ -1,12 +1,18 @@
 import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from  'react-router-dom';
 import HomePage from './pages/HomePage'
 import MainLayout from './layouts/MainLayout';
+import JoinUs from './pages/JoinUs';
+import JoinCaptain from './pages/JoinCaptain';
+import MapView from './components/MapView';
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element= {<MainLayout />}>
       <Route index element={<HomePage />}/>
+      <Route path='/join' element={<JoinUs />}/>
+      <Route path='/join/captain' element={<JoinCaptain />}/>
+      <Route path='/mapview' element={<MapView />}/>
     </Route>
   )
 );
