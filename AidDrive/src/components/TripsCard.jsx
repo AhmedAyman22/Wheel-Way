97651952a00a63f77afc4fc167f91f25abc907ef
AhmAyman <Ahm.Ayman4566@gmail.com>
2024-05-19 20:50:53 +0300

    Trip History done. To-do: Edit Profile page

--- AidDrive/src/components/TripsCard.jsx
+++ AidDrive/src/components/TripsCard.jsx
@@ -3,15 +3,17 @@ import basicRide from '../assets/images/basicRide_icon.png';
 import vanRide from '../assets/images/vanRide_icon.png';
 import vipRide from '../assets/images/vipRide_icon.png';
 
-var rideClass = vanRide;
+var rideClass = 'basic';
 const TripsCard = () => {
   return (
-    <div className='h-[100px] fixed w-[25%] bg-secondary rounded-[5px] mt-10 ml-10'>
-        <img src={rideClass == 'basic'? basicRide:vanRide} className='h-[50px] relative left-[5%] top-[50%] -translate-y-1/2'  />
-        <p className='relative bottom-[45%] left-[40%] font-bold'>Trip Destination</p>
-        <p className='relative bottom-[45%] text-[14px] left-[40%] '>Date&Time</p>
-        <p className='relative bottom-[45%] left-[40%] font-bold'>Trip Destination</p>
-        <p className='relative bottom-[45%] left-[40%] font-bold'>Trip Destination</p>
+    <div className='relative h-[100px] w-[35%] bg-secondary rounded-[10px] mt-8 ml-10 hover:opacity-90 hover:scale-110 hover:-translate-y-1'>
+        <img src={rideClass === 'basic' ? basicRide : rideClass === 'van' ? vanRide : rideClass === 'vip' ? vipRide : null}className='h-[40px] relative left-[5%] top-[50%] -translate-y-1/2'  />
+        <div className='left-[35%]  ml-5'>
+          <p className='relative left-[35%] bottom-8 font-bold'>Trip Destination</p>
+          <p className='relative text-[14px] bottom-8 left-[40%]'>Date&Time</p>
+          <p className='relative text-[14px] bottom-8 left-[40%]'>Price</p>
+          <p className='relative text-[14px] bottom-8 left-[40%] '>Status</p>
+        </div>
     </div>
   )
 }
