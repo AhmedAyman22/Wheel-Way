02eae32a8dde3dbe22fc5722caf917a743b94dde
AhmAyman <Ahm.Ayman4566@gmail.com>
2024-05-31 01:44:32 +0300

    Changed the name back to Wheel Way, added the trip hunting page and conditional rendering for the navbar and homepage

--- Wheel Way/src/pages/HomePage.jsx
+++ Wheel Way/src/pages/HomePage.jsx
@@ -1,15 +1,17 @@
 import hero from '../assets/images/homepage-hero.jpg'
 import {Link} from 'react-router-dom';
+import React,{useState} from 'react';
 
 const Home = () => {
+  const [loggedIn, setLloggedIn] = useState(true);
   return (
     <>
     <div className=''>
-    <h1 className="text-[36px] font-bold flex items-center justify-center h-auto sm:text-[48px] mt-[30px] text-primary">AIDDRIVE</h1>
+    <h1 className="text-[36px] font-bold flex items-center justify-center h-auto sm:text-[48px] mt-[30px] text-primary">WHEEL WAY</h1>
     <h2 className="text-[36px] sm:text-[48px] font-bold relative inset-y-[60px] justify-end ml-[10px] sm:left-[80px] text-primary">Connecting People, One Ride at a Time</h2>
     
     <p className="sm:text-[20px] text-primary/75 relative top-[80px] text-[16px] justify-end ml-[20px] sm:left-[130px] whitespace-pre; w-[900px] h-[120px]">
-      Welcome to AidDrive, where mobility knows no bounds. 
+      Welcome to Wheel Way, where mobility knows no bounds. 
       <br></br>We’re more than just a website; we’re a community dedicated to ensuring 
       <br></br>that everyone can move freely and independently. Our mission is simple: 
       <br></br>to provide safe, reliable transportation for physically challenged individuals.
@@ -24,13 +26,13 @@ const Home = () => {
     <li className='text-balance w-[450px] mb-[10px] sm:mb-[0px] sm:text-nowrap'>Trained Drivers: Our compassionate drivers undergo rigorous training to assist passengers
     with care and respect.<br></br> Whether it’s a medical appointment, a social outing, or a simple trip to
     the grocery store, we’ve got you covered.</li>
-    <li className='text-balance w-[450px] mb-[10px] sm:mb-[0px] sm:text-nowrap'>On-Demand Service: Need a ride? Just open the website!<br></br> AidDrive connects you
+    <li className='text-balance w-[450px] mb-[10px] sm:mb-[0px] sm:text-nowrap'>On-Demand Service: Need a ride? Just open the website!<br></br> Wheel Way connects you
     with nearby drivers who understand your unique needs. No waiting, no hassle.</li>
     </ul>
     </div>
 
     <div className='text-[15px] text-primary font-medium  relative top-[120px] justify-end right-[160px] sm:left-[0px] '>
-    <p className='relative left-[170px]'>Why Choose AidDrive:</p>
+    <p className='relative left-[170px]'>Why Choose Wheel Way:</p>
     <ul className='list-inside list-disc relative left-[190px] justify '>
     <li className='text-balance mb-[10px] sm:mb-[0px] sm:text-nowrap'>Reliability: We’re here when you need us, rain or shine. Count on us for timely pickups and drop-offs.</li>
     <li className='text-balance mb-[10px] sm:mb-[0px] sm:text-nowrap'>Community: Join a supportive community of riders, drivers, and caregivers who share a common goal:<br></br>  enhancing mobility for all.</li>
@@ -40,8 +42,12 @@ const Home = () => {
     </div>
     <img src={hero} className='sm:visible invisible w-[650px] h-[600px] rounded-[20px] absolute top-[230px] left-[1100px]'/>
     <div className=' hover:-translate-y-1 hover:text-whitish hover:scale-110 transition ease-in-out delay-150 w-[200px] h-[60px] bg-accent text-primary font-bold hover:font-bold text-[24px]  rounded-[5px] absolute sm:bottom-[50px] left-[220px] bottom-[-280px] sm:left-[500px]'>
+      {loggedIn?
+      <Link to="/booking" className='relative top-[20%] left-[15%] '>BOOK NOW</Link>:
       <Link to="/join" className='relative top-[20%] left-[25%] '>JOIN US</Link>
-    </div>
+      
+    }
+      </div>
     </div>
     
     </>
